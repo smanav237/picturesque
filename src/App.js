b37b9b3795b219dc68cd52982fd23d564db107ef
yash <yashmangla123456@gmail.com>
2023-01-05 15:55:20 +0530

    Signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+// import Navbar from './components/Navbar/Navbar';
 
 import './App.css';
 import Landing from './components/Landing/Landing';
 
 const App = () => (
   <div>
-    <Landing/>
+    <Landing />
+
   </div>
 );
 
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,31 +1,38 @@
 import React from 'react'
+import { render } from 'react-dom';
 import landing_profile from "../images/landing_profile.jpg"
 import landing_logo from "../images/image17.PNG"
 import './Landing.css';
+import Login from '../Login/Login'
+function Lgn_click() {
+  const addrss = document.getElementById('landing_user');
+  render(<Login />, addrss);
+}
+
 
 const Landing = () => {
   return (
     <div className='landing_bg'>
-     <div className="navbar_logo">
-      <img src={landing_logo} alt="" />
-    </div>
-      
+      <div className="navbar_logo">
+        <img src={landing_logo} alt="" />
+      </div>
+
       {/* <div className="landing_content"> */}
-        {/*
+      {/*
         <div className='landing_profile'>
            <img className='landing_profilpic' src={landing_profile} alt="" /> 
             <h3 className='quote'>Unleash Your Creativity, Share your designs,arts etc</h3>
         </div>
          */}
-        
 
-        <div className='landing_user'>
-        <h4 className='quote'>HAPPINESS IS...<br/>CAPTURING SPECIAL MOMENTS<br/>WITH YOUR CAMERA.</h4>
-        <button type="button" className='custom__button'>Login</button>
+
+      <div className='landing_user' id="landing_user">
+        <h4 className='quote'>HAPPINESS IS...<br />CAPTURING SPECIAL MOMENTS<br />WITH YOUR CAMERA.</h4>
+        <button type="button" className='custom__button' onClick={Lgn_click}>Login</button>
         <button type="button" className='custom__button'>SignUp</button>
-        </div>
+      </div>
 
-    {/* </div> */}
+      {/* </div> */}
 
     </div>
   )
