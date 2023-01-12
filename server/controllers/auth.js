729a329c459f9b56249ab7f43fc4fb76dd96f441
Manav <manvashishth19@gmail.com>
2023-01-12 16:46:53 +0530

    local commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import { Jwt } from "jsonwebtoken";
+import User from "../models/User.js"
+
+// Register User
+export const register = async(req,res) =>{
+    try{
+
+    } catch (err) {
+
+    }
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path";
 import {fileURLToPath} from "url";
+import {register} from "controllers/auth.js"
 
 // configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,9 @@ const storage = multer.diskStorage({
     },
   });
   const upload = multer({ storage });
+
+  // Routes with files
+  app.post("/auth/register", upload.single("picture"),register);
 
   /* MONGOOSE SETUP */
 const PORT = process.env.PORT;
